634426de679d6c684c736f0bb2bac546425e66ad
Amrutha Kodad <amruthakodad09@gmail.com>
2024-06-05 15:10:38 +0530

    appearance mixin experimentation-3

--- src/ComponentComposition/AppearanceMixin.ts
+++ src/ComponentComposition/AppearanceMixin.ts
@@ -1,5 +1,6 @@
-import { LitElement, css, html } from 'lit';
-import { property, state } from 'lit/decorators.js';
+import { LitElement, css, html} from 'lit';
+import { property} from 'lit/decorators.js';
+// import { styleMap } from 'lit/directives/style-map.js';
 
 // constructor-type.ts
 type Constructor<T = {}> = new (...args: any[]) => T;
@@ -24,13 +25,6 @@ export const AppearanceMixin = <T extends Constructor<LitElement>>(superClass: T
     @property({ type: String }) buttonBGColor = '#5D3FD3';
     @property({ type: String }) buttonHoverColor = '#7D65DB';
 
-    @state() private tempColor = this.color;
-    @state() private tempBackgroundColor = this.backgroundColor;
-    @state() private tempTaskItemBGColor = this.taskItemBGColor;
-    @state() private tempButtonTextColor = this.buttonTextColor;
-    @state() private tempButtonBGColor = this.buttonBGColor;
-    @state() private tempButtonHoverColor = this.buttonHoverColor;
-
     static styles = [
       (superClass as any).styles || css``, // Include superClass styles if any or else keep it empty
       css`
@@ -41,13 +35,6 @@ export const AppearanceMixin = <T extends Constructor<LitElement>>(superClass: T
           --appearance-button-text-color: white;
           --appearance-button-bg-color: #5D3FD3;
           --appearance-button-hover-bg-color: #7D65DB;
-        }
-
-        .inputField{
-            display: flex;
-            padding: 15px;
-            background-color: var(--appearance-background-color);
-            border-radius: 5px;
         }
       `,
     ];
@@ -74,56 +61,14 @@ export const AppearanceMixin = <T extends Constructor<LitElement>>(superClass: T
       }
     }
 
-    handleInputChange(event: Event) {
-      const target = event.target as HTMLInputElement;
-      const property = target.name as keyof this;
-      (this[property] as unknown as string) = target.value;
-    }
-
-    applyChanges() {
-      this.color = this.tempColor;
-      this.backgroundColor = this.tempBackgroundColor;
-      this.taskItemBGColor = this.tempTaskItemBGColor;
-      this.buttonTextColor = this.tempButtonTextColor;
-      this.buttonBGColor = this.tempButtonBGColor;
-      this.buttonHoverColor = this.tempButtonHoverColor;
-    }
-
     renderAppearance(content: unknown) {
-      return html`
-        <div>
-          <div class="inputField">
-            <label>
-              Text Color:
-              <input type="text" name="tempColor" .value="${this.tempColor}" @input="${this.handleInputChange}">
-            </label>
-            <label>
-              Background Color:
-              <input type="text" name="tempBackgroundColor" .value="${this.tempBackgroundColor}" @input="${this.handleInputChange}">
-            </label>
-            <label>
-              Task Item Background Color:
-              <input type="text" name="tempTaskItemBGColor" .value="${this.tempTaskItemBGColor}" @input="${this.handleInputChange}">
-            </label>
-            <label>
-              Button Text Color:
-              <input type="text" name="tempButtonTextColor" .value="${this.tempButtonTextColor}" @input="${this.handleInputChange}">
-            </label>
-            <label>
-              Button Background Color:
-              <input type="text" name="tempButtonBGColor" .value="${this.tempButtonBGColor}" @input="${this.handleInputChange}">
-            </label>
-            <label>
-              Button Hover Color:
-              <input type="text" name="tempButtonHoverColor" .value="${this.tempButtonHoverColor}" @input="${this.handleInputChange}">
-            </label>
-            <button @click="${this.applyChanges}">Apply</button>
-          </div>
-          ${content}
-        </div>
-      `;
-    }
+        return html`
+          <div >
+            ${content}
+          </div>`;
+        }
+      
   }
 
-  return Appearance as Constructor<AppearanceInterface> & T;
+  return Appearance ;
 };
